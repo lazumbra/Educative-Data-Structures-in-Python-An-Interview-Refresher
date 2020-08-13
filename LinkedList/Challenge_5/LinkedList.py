7e57b2767c763df8401f4ae099283e6f52b130b6
Jefferson de Sousa <lazumbra@gmail.com>
2020-08-12 21:55:41 -0300

    add element at head

--- LinkedList/Challenge_5/LinkedList.py
+++ LinkedList/Challenge_5/LinkedList.py
@@ -16,3 +16,17 @@ class LinkedList:
 
     def insert_at_head(self, dt):
         # Para inserir eu preciso de uma variável temporal
+        # porque eu preciso dessa variável temp?
+        # Eu conseguiria adicionar código sem essa variável?
+        temp_node = Node(dt)
+        # Porque eu tenho de chamar o self para chamar uma função aqui dentro?
+        if(self.is_empty()):
+            self.head_node = temp_node
+            return self.head_node
+        # Se a lista não estiver vazia.
+        temp_node.next_element = self.head_node
+        # Eu preciso mesmo de alocar o previous elemento para temp_node?
+        # Fazer um teste do que aconteceria se eu não fizesse essa alocação.
+        self.head_node.previous_element = temp_node
+        self.head_node = temp_node
+        return self.head_node
