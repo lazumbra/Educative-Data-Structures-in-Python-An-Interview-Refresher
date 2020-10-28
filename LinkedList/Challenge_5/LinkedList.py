8667ec3456734de12e80cd9a7fabadec98a5e5bc
Jefferson de Sousa <lazumbra@gmail.com>
2020-10-28 18:51:28 -0300

    função para adicionar um nó no fim da lista

--- LinkedList/Challenge_5/LinkedList.py
+++ LinkedList/Challenge_5/LinkedList.py
@@ -30,3 +30,31 @@ class LinkedList:
         self.head_node.previous_element = temp_node
         self.head_node = temp_node
         return self.head_node
+
+    def insert_at_tail(self, value):
+        # crio um novo nó
+        new_node = Node(value)
+
+        # Olhar se a lista for vazia. Se a lista for vazia inserir o elemento
+        # Como sendo a cabeça
+        if self.head_node is None:
+            self.head_node = new_node
+            return
+
+        # Se a lista não for vazia eu vou percorrer a lista e parar no último elemento
+        # Perceba que eu paro no último elemento e não no None
+        # Para percorrer a lista eu vou criar uma variável temporária.
+        """
+        A variável temporária vai servir como o apontador para eu ter acesso ao próximo elemento.
+        Ela também vai ser para eu fazer a troca de nó
+        """
+        # Criando a variável temporária
+        temp_node = self.get_head()
+
+        while temp_node.next_element is not None:
+            temp_node = temp_node.next_element
+
+        # Agora que eu cheguei no último elemento é hora de adicionar
+        # o meu elmento objetivo.
+        temp_node.next_element = new_node
+        return
