60ed5b5dcfecb93da77dd486feeb9396acf8c7ed
Jefferson de Sousa <lazumbra@gmail.com>
2020-11-10 19:58:56 -0300

    adding node at the tail

--- LinkedList/Challenge_6/main.py
+++ LinkedList/Challenge_6/main.py
@@ -1,8 +1,8 @@
 from LinkedList import LinkedList
 
 minha_lista = LinkedList()
-minha_lista.insert_at_head(1)
-minha_lista.insert_at_head(2)
-minha_lista.insert_at_head(3)
+minha_lista.insert_at_tail(1)
+minha_lista.insert_at_tail(2)
+minha_lista.insert_at_tail(3)
 
 minha_lista.print_list()
