f50e549eca9b6b23d219ff675fc74d1892e021fb
Jefferson de Sousa <lazumbra@gmail.com>
2020-12-10 21:02:14 -0300

    test the new function to detecl a loop

--- LinkedList/Challenge_6/main.py
+++ LinkedList/Challenge_6/main.py
@@ -5,11 +5,16 @@ minha_lista = LinkedList()
 minha_lista.insert_at_tail(1)
 minha_lista.insert_at_tail(2)
 minha_lista.insert_at_tail(3)
+minha_lista.insert_at_tail(1)
+
 
 minha_lista.print_list()
+print(minha_lista.detect_loop())
 
 # minha_lista.delete_at_head()
 
-print(minha_lista.delete(3))
+# print(minha_lista.delete(9))
 
-minha_lista.print_list()
+# minha_lista.print_list()
+
+# print(minha_lista.length())
